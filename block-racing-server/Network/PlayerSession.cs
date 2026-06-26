4159b880d1af117545c028dbb61c7f523e39b08c
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-06-26 16:10:50 +0900

    feat: 연결 시 클라이언트별 세션 실행

---
+++ block-racing-server/Network/PlayerSession.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace block_racing_server.Network;
+
+public class PlayerSession
+{
+    private readonly TcpClient _client;
+    private readonly NetworkStream _stream;
+
+    public PlayerSession(TcpClient client)
+    {
+        _client = client;
+        _stream = client.GetStream();
+    }
+
+    public async Task StartAsync()
+    {
+        Console.WriteLine($"PlayerSession 시작 : {_client.Client.RemoteEndPoint}");
+
+        await ReceiveLoopAsync();
+    }
+
+    private async Task ReceiveLoopAsync()
+    {
+        byte[] buffer = new byte[1024];
+
+        try
+        {
+            while (true)
+            {
+                int received = await _stream.ReadAsync(buffer);
+
+                if (received == 0)
+                    break;
+
+                string message = Encoding.UTF8.GetString(buffer, 0, received);
+
+                Console.WriteLine($"Receive : {message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Disconnect();
+        }
+    }
+
+    public async Task SendAsync(string message)
+    {
+        byte[] data = Encoding.UTF8.GetBytes(message);
+
+        await _stream.WriteAsync(data);
+    }
+
+    private void Disconnect()
+    {
+        _stream.Close();
+        _client.Close();
+    }
+}
+
+
---
+++ block-racing-server/Network/TcpServer.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace block_racing_server.Network;
+
+public class TcpServer
+{
+    private TcpListener? _listener;
+
+    public async Task StartAsync(int port)
+    {
+        _listener = new TcpListener(IPAddress.Any, port);
+        _listener.Start();
+
+        Console.WriteLine($"서버 시작 : {port}");
+
+        try
+        {
+            while (true)
+            {
+                TcpClient client = await _listener.AcceptTcpClientAsync();
+
+                PlayerSession session = new PlayerSession(client);
+                _ = session.StartAsync(); // 세션 시작
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"서버 에러: {ex.Message}");
+            return;
+        }
+        finally
+        {
+            _listener.Stop();
+        }
+
+    }
+}
