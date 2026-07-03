d82bd685e548dfd811a707392c74b093a865072f
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-07-03 21:21:50 +0900

    refactor: 패킷 핸들러 제네릭 Register 방식으로 변경

---
+++ block-racing-server/Network/Handler/ChatHandler.cs
@@ -0,0 +1,12 @@
+﻿using block_racing_server.Network;
+using block_racing_server.Network.Packet;
+
+namespace block_racing_server.Network.Handler;
+
+public static class ChatHandler
+{
+    public static void Handle(PlayerSession session, CChatPacket packet)
+    {
+        Console.WriteLine(packet.Message);
+    }
+}
