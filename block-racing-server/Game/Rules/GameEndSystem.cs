92cc583ada167f3d13cc5c0491cfe5a83e4e4c81
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-08-01 14:00:10 +0900

    feat: 무승부 처리 추가

--- block-racing-server/Game/Rules/GameEndSystem.cs
+++ block-racing-server/Game/Rules/GameEndSystem.cs
@@ -5,23 +5,33 @@ namespace block_racing_server.Game.Rules;
 
 public class GameEndSystem
 {
-    public GameEndResult Update(GameState gameState)
+    public GameEndResult? Update(GameState gameState)
     {
-        foreach (Player player in gameState.Players.Values)
-        {
-            Console.WriteLine($"Player {player.Id} distance: {player.Car.Distance}");
+        Player[] finishedPlayers = gameState.Players.Values
+            .Where(player =>
+                player.Car.Distance >= gameState.TargetDistance)
+            .ToArray();
 
-            if (player.Car.Distance >= gameState.TargetDistance)
-            {
-                Player loser = gameState.Players.Values
-                                .First(p => p.Id != player.Id);
+        // 아무도 결승선에 도달하지 않음
+        if (finishedPlayers.Length == 0)
+            return null;
 
-                return new GameEndResult(
-                    player,
-                    loser);
-            }
-        }
+        // 두 플레이어가 동시에 결승선 도달
+        if (finishedPlayers.Length == 2)
+            return new GameEndResult(
+                winner: null,
+                loser: null
+            );
 
-        return null;
+        // 한 명만 결승선 도달
+        Player winner = finishedPlayers[0];
+
+        Player loser = gameState.Players.Values
+            .First(player => player.Id != winner.Id);
+
+        return new GameEndResult(
+            winner,
+            loser
+        );
     }
 }
