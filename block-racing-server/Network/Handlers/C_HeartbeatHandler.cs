371a3b268e6a23530b7a58b2c881f627e81fbcbf
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-08-23 17:02:51 +0900

    feat: Heartbeat 및 Timeout 추가

---
+++ block-racing-server/Network/Handlers/C_HeartbeatHandler.cs
@@ -0,0 +1,13 @@
+﻿using block_racing_common.Network.Packets;
+
+namespace block_racing_server.Network.Handlers;
+
+public static class C_HeartbeatHandler
+{
+    public static void Handle(PlayerSession session, C_HeartbeatPacket packet)
+    {
+        session.UpdateHeartbeat();
+
+        _ = session.SendAsync(new S_HeartbeatPacket());
+    }
+}
--- block-racing-server/Network/SessionManager.cs
+++ block-racing-server/Network/SessionManager.cs
@@ -50,4 +50,19 @@ public class SessionManager
             await session.SendAsync(data);
         }
     }
+
+    public async Task UpdateAsync()
+    {
+        foreach (var session in _sessions.Values)
+        {
+            if (session.IsHeartbeatTimeout())
+            {
+                Console.WriteLine(
+                    $"Heartbeat Timeout : {session.Id}"
+                );
+
+                await session.DisconnectAsync();
+            }
+        }
+    }
 }
